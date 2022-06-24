9990fde41032b2dc12cea0fa683a3277f1016b88
mirra07 <85962313+mirra07@users.noreply.github.com>
2022-06-24 22:38:24 +0530

    Add files via upload

---
+++ day16/deck of cards.py
@@ -0,0 +1,6 @@
+import random as r
+suits=['Hearts','Diamonds','Spades','Clubs']
+number=['1','2','3','4','5','6','7','8','9','10','A','K','Q','J']
+sel_suits=r.choice(suits)
+sel_number=r.choice(number)
+print(sel_suits,sel_number)
